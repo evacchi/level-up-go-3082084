a883ca584f22b1d3cce19dc9e82ae6e10d53f9e3
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-12-13 14:48:03 +0000

    heap

--- 01_09b/main.go
+++ 01_09b/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -17,9 +18,36 @@ type Song struct {
 	PlayCount int64  `json:"play_count"`
 }
 
+type Playlist []Song
+
+func (p Playlist) Len() int           { return len(p) }
+func (p Playlist) Less(i, j int) bool { return p[i].PlayCount > p[j].PlayCount }
+func (p Playlist) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func (p *Playlist) Push(s any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*p = append(*p, s.(Song))
+}
+
+func (p *Playlist) Pop() any {
+	old := *p
+	n := len(old)
+	x := old[n-1]
+	*p = old[0 : n-1]
+	return x
+}
+
 // makePlaylist makes the merged sorted list of songs
 func makePlaylist(albums [][]Song) []Song {
-	panic("NOT IMPLEMENTED")
+	p := &Playlist{}
+	heap.Init(p)
+	for _, album := range albums {
+		for _, song := range album {
+			heap.Push(p, song)
+		}
+	}
+	return *p
 }
 
 func main() {
